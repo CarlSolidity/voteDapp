b5e51749c981deb6c9e030c67be3aeb0e4025c5b
CarlSolidity <carl.charpin@protonmail.com>
2021-08-24 23:14:12 -0400

    First commit

---
+++ contracts/OneVote.sol
@@ -0,0 +1,8 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// Create a contract that takes one vote from each voter, returns the winning candidate.
+contract OneVote {
+
+}
