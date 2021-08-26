f5f104bdd8e0b3d9999c135feb8be42077fb94bc
CarlSolidity <carl.charpin@protonmail.com>
2021-08-25 23:30:18 -0400

    Started vote contract

--- contracts/OneVote.sol
+++ contracts/OneVote.sol
@@ -2,7 +2,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-// Create a contract that takes one vote from each voter, returns the winning candidate.
+// Inspired by solidity docs
 contract OneVote {
+    
+    // characteristics of a voter
+    struct Voters {
+        uint weight; //used to determine if the address can vote
+        bool voted; // Check if voted already or not
+        uint vote;
+    }
+
+    // proposals
+    struct Proposal {
+        bytes32 name;
+        uint voteCount; // how many votes received
+    }
+
+    address public _owner;
+
+    // mapping of address to associate characteristics to an address
+    mapping(address => Voters) public voters;
+
+    Proposal[] public proposals;
 
 }
