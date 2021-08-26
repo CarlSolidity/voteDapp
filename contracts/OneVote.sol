e822a33d43d68e3c5cc9eab177f46ee42e2dafd4
CarlSolidity <carl.charpin@protonmail.com>
2021-08-26 19:48:55 -0400

    Vote contract done. Environment reinstalled.

--- contracts/OneVote.sol
+++ contracts/OneVote.sol
@@ -25,4 +25,58 @@ contract OneVote {
 
     Proposal[] public proposals;
 
+    modifier isOwner() {
+        require(msg.sender == _owner, "Only the owner can use this function");
+        _;
+    }
+
+    // Feed an array to the constructor and it creates as 
+    // many proposal object as candidates named in the array
+    constructor(bytes32[] memory proposalNames) {
+        _owner = msg.sender;
+        voters[_owner].weight = 1;
+
+        for (uint i=0; i<proposalNames.length; i++) {
+            proposals.push(Proposal({
+                name: proposalNames[i],
+                voteCount:0
+            }));
+        }
+    }
+
+    // One transaction needed for each time you add a voting address
+    function giveRightToVote(address voter) public isOwner {
+        require(!voters[voter].voted, "This address already voted");
+        require(voters[voter].weight == 0);
+        voters[voter].weight = 1;        
+    }
+
+    // Cast a vote if requirements are met
+    function vote(uint proposal) public {
+        Voters storage sender = voters[msg.sender];
+        require(sender.weight !=0, "No right to vote");
+        require(!sender.voted, "This address already voted");
+        sender.voted = true;
+        sender.vote = proposal;
+
+        proposals[proposal].voteCount += sender.weight;
+
+    }
+
+    // Computes the winner of the votes (most votes for the proposal)
+    // Does not take equal winners in account.
+    function winningProposal() public view returns (uint winningProposal_) {
+        uint winningVoteCount = 0;
+        for(uint p=0; p<proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+
+    // returns the name of the winning proposal
+    function winnerName() public view returns (bytes32 winnerName_) {
+        winnerName_ = proposals[winningProposal()].name;
+    }
 }
